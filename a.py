c051d2cf1ea16b4c9673e1660c8adc8470c3f96a
Miras Maratov <98344240+mrsmrtv@users.noreply.github.com>
2025-01-22 10:12:36 +0500

    Add files via upload

---
+++ a.py
@@ -0,0 +1,23 @@
+print("Hello World")
+a=5 
+c=6
+print(a+c)
+#This is a comment
+x=20
+print (type(x))
+text="This is a text"
+print(text[1:5])
+print(text.upper)
+print(text.lower)
+text2="!"
+print(text+text2)
+tuple=(1,2,3)
+print(tuple)
+print(tuple[1])
+tuple2=(2,5,6)
+print(tuple+tuple2)
+g=9
+h=8
+if g<h:
+    h+=1
+print(h)
